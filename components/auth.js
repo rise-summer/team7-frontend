22a0d0272a875b6e256d7fee228669481fcd3d34
Joshua Kaplan <joshuagraykaplan@gmail.com>
2020-08-13 03:50:39 -0500

    changed something?

--- components/auth.js
+++ components/auth.js
@@ -1,44 +1,55 @@
-import { AsyncStorage } from 'react-native'
-import axios from 'axios';
+import { AsyncStorage } from "react-native";
+import axios from "axios";
 
-function login(email, password){
-  return axios.post('http://localhost:3000/login', {
-    'email': email,
-    'password': password,
-  }).then((res) => {
-    if(res.error){
-      console.log(res.error);
-    } else {
-      AsyncStorage.setItem('jwt', res.token);
-    }
-  }).catch((err) => {
-    console.log(err);
-  });
+function login(email, password) {
+  return axios
+    .post("http://localhost:3000/login", {
+      email: email,
+      password: password,
+    })
+    .then((res) => {
+      if (res.error) {
+        console.log(res.error);
+      } else {
+        AsyncStorage.setItem("jwt", res.token);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-function signup(data){
-  axios.post('http://localhost:3000/signup', {
-    'firstname': profile.firstname,
-    'lastname': profile.lastname,
-    'email': profile.email,
-    'password': profile.password
-  }, {withCredentials: true}).then((res) => {
-    console.log(res);
-  }).catch((err) => {
-    console.log(err);
-  }).then(() => {
-    navigation.navigate('Home');
-  });
+function signup(data) {
+  axios
+    .post(
+      "http://localhost:3000/signup",
+      {
+        firstname: profile.firstname,
+        lastname: profile.lastname,
+        email: profile.email,
+        password: profile.password,
+      },
+      { withCredentials: true }
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .then(() => {
+      navigation.navigate("Home");
+    });
 }
 
-function logout(){
-  AsyncStorage.removeItem('jwt');
+function logout() {
+  AsyncStorage.removeItem("jwt");
 }
 
 //use is not necessarily authenticated, but has a token
 //that will be validated on request
-function isAuthenticated(){
-  return AsyncStorage.getItem('jwt') != null;
+function isAuthenticated() {
+  return AsyncStorage.getItem("jwt") != null;
 }
 
 export default {};
